55c2f4bd9b572ade3f9a87763a4c81b0c3cddaaf
somesortoferror <somesortoferror@gmail.com>
2016-02-21 21:06:46 +0000

    bitrate chart

--- README.md
+++ README.md
@@ -3,11 +3,12 @@ Specton Audio Analyser
 
 Graphical frontend for various audio scanners (mp3guessenc etc)
 
-Requires Pyqt5
+Requires Python 3, PyQt5, matplotlib
 
-Windows: Use the binary installers here: https://riverbankcomputing.com/software/pyqt/download5
+Windows: The easiest way to satisfy these requirements is to install [WinPython](http://winpython.github.io/#releases). Be sure to choose an installer with PyQt5 included (These have 'Qt5' in the filename)
 
-Ubuntu: sudo apt-get install python3-pyqt5 
+Debian/Ubuntu: sudo apt-get install python3-pyqt5 python-matplotlib
 
 This product uses [MediaInfo](http://mediaarea.net/MediaInfo) library, Copyright (c) 2002-2014 MediaArea.net SARL.
+
 Uses IcoMoon Free Pack icons (https://icomoon.io/)
---
+++ spcharts.py
@@ -0,0 +1,30 @@
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from PyQt5.QtWidgets import QSizePolicy
+
+class Bitrate_Chart(FigureCanvas):
+    def __init__(self, parent=None, width=5, height=4, dpi=100, x=[], y=[]):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure(x,y)
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QSizePolicy.Expanding,
+                                   QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self,x,y):
+         self.axes.bar(x,y,width=20,align='center')
+         self.axes.set_ylabel('Number of frames')
+         self.axes.set_xlabel('Bitrate (Kbps)')
+         self.axes.set_xticks([32,64,96,128,160,192,224,256,288,320,352,384])
+        
